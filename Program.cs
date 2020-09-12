77c2344c9009bb9801ad14e578e18555503d0245
vitoriahelen-git <vitoriahellen3004@gmail.com>
2020-09-12 00:57:19 -0300

    Resolução de exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase; 
+            Console.Write("Digite uma frase: ");
+            frase = Console.ReadLine(); 
+            Console.WriteLine("\n" + frase.Replace("r","l")); 
+
+        }
+    }
+}
